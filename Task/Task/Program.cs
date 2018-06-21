c970b654278d20df436983c912236f0aa52b607e
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-06-21 19:28:00 +0300

    Add primary data read method and primary data file

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task
 {
@@ -6,7 +7,32 @@ namespace Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text;
+            int symbolsCountInRow;
+
+            ReadFile("duomenys.txt", out text, out symbolsCountInRow);
+        }
+
+        public static void ReadFile(string fileName, out string text, out int symbolsCountInRow)
+        {
+            text = "";
+            symbolsCountInRow = 0;
+
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+
+                text = lines[0];
+                symbolsCountInRow = Convert.ToInt32(lines[1]);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error. File not found!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error. Wrong file format!");
+            }
         }
     }
 }
