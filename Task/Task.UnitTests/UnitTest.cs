2ff2878c898077f88736d4f1292f6b7fc141205c
SC\minsma <mindaugas.smalinskas@gmail.com>
2018-06-22 00:22:21 +0300

    Created tests for the rest of methods

--- Task/Task.UnitTests/UnitTest.cs
+++ Task/Task.UnitTests/UnitTest.cs
@@ -30,5 +30,88 @@ namespace Task.UnitTests
         {
             (string text, int symbolsCountInRow) = Program.ReadFile("wrong_data_for_test.txt");
         }
+
+        [TestMethod]
+        public void SplitTextIntoParts_IfWorks_ReturnsListWithValues()
+        {
+            List<string> textInPairs = Program.SplitTextIntoParts("þodis þodis þodis", 13);
+
+            Assert.AreEqual(2, textInPairs.Count);
+        }
+
+        [TestMethod]
+        public void PrintToFile_IfPrints_CreatesResultsFile()
+        {
+            List<string> values = new List<string> { "abc", "adc", "bde", "dea" };
+
+            Program.PrintToFile("rez.txt", values);
+
+            string[] lines = File.ReadAllLines("rez.txt");
+
+            Assert.AreEqual(4, lines.Length);
+        }
+
+        [TestMethod]
+        public void ReduceWordAndAddWordToList_IfWorks_ReducesWordAndPutItInList()
+        {
+            List<string> values = new List<string>();
+
+            string word = Program.ReduceWordAndAddWordToList(values, "abbbbbbbbcd", 5);
+
+            Assert.AreEqual("abbbb ", values[0]);
+            Assert.AreEqual(6, word.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ReduceWordAndAddWordToList_IfFails_GetException()
+        {
+            List<string> values = new List<string>();
+
+            string word = Program.ReduceWordAndAddWordToList(values, "abc", 5);
+        }
+
+        [TestMethod]
+        public void AddWordToPartsListWhileReducingWordLength_IfWorks_ReturnsWordSplittedIntoList()
+        {
+            List<string> values = new List<string>();
+            string word = "abbccccddddd";
+
+            Program.AddWordToPartsListWhileReducingWordLength(values, word, 3);
+
+            Assert.AreEqual(4, values.Count);
+        }
+
+        [TestMethod]
+        public void FirstWordProcessing_IfWorks_IncreaseList()
+        {
+            List<string> words = new List<string>() { "zodiakas" };
+            List<string> parts = new List<string>();
+
+            Program.FirstWordProcessing(0, words, parts, 5);
+
+            Assert.AreEqual(2, parts.Count);
+        }
+
+        [TestMethod]
+        public void WordsAfterFirstWordProcessing_IfPartsListNotEmpty_IncreaseList()
+        {
+            List<string> words = new List<string>() { "zodiakas" };
+            List<string> parts = new List<string>() { "la " };
+
+            Program.WordsAfterFirstWordProcessing(0, words, parts, 5);
+
+            Assert.AreEqual(3, parts.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WordsAfterFirstWordProcessing_IfPartsListIsEmpty_IncreaseList()
+        {
+            List<string> words = new List<string>() { "zodiakas" };
+            List<string> parts = new List<string>();
+
+            Program.WordsAfterFirstWordProcessing(0, words, parts, 5);
+        }
     }
 }
